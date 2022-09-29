94af86da1615bccb3a4fbac942c3b1067cba833e
Dylan Roets <dylanroets@yahoo.com>
2022-09-29 15:04:17 -0500

    added car values, cleaned up cars and bikes display

--- numbers.js
+++ numbers.js
@@ -1,10 +1,17 @@
 console.log( 'js' );
 
 let motorcycles = 9;
-
+let cars = 0;
 console.log( 'bikes:', motorcycles);
+console.log( 'cars:', cars); 
 console.log( 'had to put the soft tail in the garage' );
 
 //decrement motorcycles
 motorcycles--;
 console.log( 'bikes:', motorcycles);
+console.log( 'cars:', cars); 
+
+console.log( 'missed the bike so I bought 2 lambos');
+cars += 2;
+console.log( 'bikes:', motorcycles);
+console.log( 'cars:', cars); 
