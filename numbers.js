49fc9c3325cd44575b59e0f065fb1b258e91bc3f
Dylan Roets <dylanroets@yahoo.com>
2022-09-29 14:51:44 -0500

    added js motorcycles values

--- numbers.js
+++ numbers.js
@@ -1,1 +1,10 @@
 console.log( 'js' );
+
+let motorcycles = 9;
+
+console.log( 'bikes:', motorcycles);
+console.log( 'had to put the soft tail in the garage' );
+
+//decrement motorcycles
+motorcycles--;
+console.log( 'bikes:', motorcycles);
